7289eb41eea3c4774405c8879c5a59737926d03f
thevirtuozz <ravenel.thomas@outlook.fr>
2023-10-18 21:31:45 +0200

    correct syntax error

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
-{php bin/console make:admin:crud
+{
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
