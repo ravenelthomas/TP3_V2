f82978ff5d6de430fa2f90a86a39f04db65c4655
thevirtuozz <ravenel.thomas@outlook.fr>
2023-10-19 17:37:42 +0200

    Update

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\UserType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class AdminController extends AbstractController
 {
@@ -17,8 +23,43 @@ class AdminController extends AbstractController
     }
     #[Route('/admin/edit_user', name: 'app_edit_user')]
 
-    public function editUser(): Response
+    public function showUser(UserRepository $userRepository): Response
     {
-        dd('edit user');
+
+        return $this->render('admin/show_user.html.twig', [
+            'controller_name' => 'AdminController',
+            'users' => $userRepository->findAll(),
+        ]);
     }
+
+    // #[Route('/admin/edit_user/{id}', name: 'app_edit_user_id')]
+    // public function modifyUser(EntityManagerInterface $entityManager, Request $request, UserRepository $userRepository, $id): Response
+    // {
+    //     $user = $userRepository->find($id);
+    
+    //     if (!$user) {
+    //         throw $this->createNotFoundException('User not found');
+    //     }
+    
+    //     $form = $this->createForm(UserType::class, $user);
+    //     $form->add('submit', SubmitType::class, [
+    //         'label' => 'Update',
+    //         'attr' => [
+    //             'class' => 'btn btn-primary',
+    //         ],
+    //     ]);
+    
+    //     $form->handleRequest($request);
+    
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $entityManager->flush();
+            
+    //         return $this->redirectToRoute('app_edit_user');
+    //     }
+    
+    //     return $this->render('user/edit.html.twig', [
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
+    
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
